0e11ac2f8a4f80929a3e43661cb86cc11bb78208
Taimooriqbal125 <taimooriqbal125@gmail.com>
2025-02-18 21:06:33 -0700

    tst

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ connectDB();
 // Initialize Express
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173", // Replace with your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 // Routes
 app.use("/", userroute);
